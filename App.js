b6d2e176b43b7e153ffdc187830efd35198046fa
vicentesantos <vicente.goncalves@fatec.sp.gov.br>
2023-03-26 18:08:45 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+  SafeAreaView
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+const App = () => {
+  <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView}>
+      <Image
+        style={styles.logo}
+        source={require('./assets/fatec-zl.png')}
+      />
+      <Text style={styles.title}>Atividade Prática 1</Text>
+      <Text style={styles.subTitle}>Vicente Santos</Text>
+    </ScrollView>
+  </SafeAreaView>
+
+};
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    paddingTop: StatusBar.currentHeight,
     flex: 1,
+  },
+  scrollView:{
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 20,
+  },
+  text:{
+    fontSize: 12,
+    alignContent: 'center'
+  },
+  logo:{
+    width: 65,
+    height: 30,
+  },
+  title: {
+    fontSize: 36,
+    color: 'white',
+  },
+  subTitle:{
+    fontSize: 20,
+    color: '#fff',
   },
 });
+
+export default App;
